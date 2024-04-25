9839a3c901d5a53fb947066be8c9654923bbdb13
Kaldea <90015672+Chaldeaaa@users.noreply.github.com>
2024-04-25 17:43:40 -0400

    Update functions.r

--- draft code/functions.r
+++ draft code/functions.r
@@ -1,16 +1,20 @@
 # Functions used for data analysis
 # craetes correlation matrix for all variables in selected regions
-# also check for p values for each pair
+# also check for adjusted p values for each pair
+# might not work outside of dataset Crime from pkg plm
 
 # Environments
 require(dplyr)
 require(tidyverse)
 require(ggplot2)
+require(stat)
 
 
 ###### Create matrix for testing ######
-matxCorr <- function(data, region){
-    # would not work outside of dataset Crime from pkg plm
+matxCorr <- function(data, region, adjust = 'bonferroni'){
+    # a function to produce a correlation matrix among selected marker of data
+    # region (region tag of county): west, central, other
+    # adjust  (p-value adjustment)(default "bonferroni"): "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none" 
     
     data <- data %>% 
         filter(region == region) %>% 
@@ -22,14 +26,16 @@ matxCorr <- function(data, region){
     mutate(test = map2(var1, var2, ~cor.test(unlist(data[.x,]),
                                         unlist(data[.y,]))),
           corr = map_dbl(test, 'estimate'),
-          p = map_dbl(test,'p.value'))
+          p_unadj = map_dbl(test,'p.value'))
+    d$p <- p.adjust(d$p_unadj, adjust)
     return(d)
 }
 
+
 ##### Organize matrix to factorial view #####
-showCorr <- function(data, type){
-    # type includes 'corr' or 'p'
-    # showing corr coef. and p values resp.
+showCorr <- function(data, type = 'p'){
+    # draw a matrix of correlation or p values
+    # type includes 'corr' or 'p' (default 'p')
     
     data %>% select(var1, var2, type) %>%
     spread(var2, type)
@@ -37,11 +43,11 @@ showCorr <- function(data, type){
 
 
 ##### Visualize matrix using ggplot heatmap #####
-paintCorr <- function(Crime, type){
-    # type includes 'corr' or 'p'
+paintCorr <- function(data, type = 'p'){
     # paint heat map based on coorelation or p values
+    # type includes 'corr' or 'p' (default 'p')
     
-    ggplot(Crime, aes(var1, var2)) + 
+    ggplot(data, aes(var1, var2)) + 
         geom_tile(aes(fill = !!as.name(type))) +
         scale_x_discrete(expand = c(0,0)) +
         scale_y_discrete(expand = c(0,0)) +
